4506589fd69b23ef5f1a41362a4d7c506cd6915f
Rick <rick@Ricks-MacBook-Pro.local>
2018-10-12 13:34:38 +0900

    Examples of types in Angular and Typescript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,21 +8,51 @@ import { Component, enableProdMode } from '@angular/core';
 
 export class UserComponent {
   // Properties
-  firstName = 'David';
-  lastName = 'Jones';
-  age = 45;
-  address = {
-    street: '100 Park Lane',
-    city: 'Beford',
-    county: 'Bedfordshire'
-  };
+  personId;
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+  foo: any;
+  isGay: boolean;
+  numArray: number[];
+  strArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
 
   // Methods
   constructor() {
+    this.firstName = 'Keith';
+    this.lastName = 'Fletcher';
+    this.age = 46;
 
+    this.address = {
+      street: '23 Pudding Lane',
+      city: 'Bedford',
+      county: 'Bedfordshire'
+    };
+
+    this.foo = 'nice';
+    this.numArray = [1, 2, 3];
+    this.strArray = ['A', 'B', 'C'];
+    this.mixedArray = [2, 'A', '67'];
+
+    this.myTuple = ['hello', 7, false];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
+
+    console.log(this.multiplyNumbers(20, 2));
   }
 
   showAge() {
     return this.age + 10;
+  }
+
+  multiplyNumbers(num1: number, num2: number): number {
+    return num1 * num2;
   }
 }
