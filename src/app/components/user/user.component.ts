5c94f22fa491801da069394abe03168cdb8524f7
Rick <rick@Ricks-MacBook-Pro.local>
2018-10-12 13:55:02 +0900

    Added users component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, enableProdMode } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component ({
   selector: 'app-user',
@@ -26,70 +27,57 @@ export class UserComponent {
 }
 
 
-interface User {
-  firstName: string;
-  lastName: string;
-  age: number;
-  address: {
-    street: string,
-    city: string,
-    state: string
-  };
-
-
-/*
-
-xport class UserComponent {
-  // Properties
-  personId;
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-  foo: any;
-  isGay: boolean;
-  numArray: number[];
-  strArray: string[];
-  mixedArray: any[];
-  myTuple: [string, number, boolean];
-  unusable: void;
-  u: undefined;
-  n: null;
-
-  // Methods
-  constructor() {
-    this.firstName = 'Keith';
-    this.lastName = 'Fletcher';
-    this.age = 46;
-
-    this.address = {
-      street: '23 Pudding Lane',
-      city: 'Bedford',
-      county: 'Bedfordshire'
-    };
-
-    this.foo = 'nice';
-    this.numArray = [1, 2, 3];
-    this.strArray = ['A', 'B', 'C'];
-    this.mixedArray = [2, 'A', '67'];
-
-    this.myTuple = ['hello', 7, false];
-    this.unusable = undefined;
-    this.u = undefined;
-    this.n = null;
-
-    console.log(this.multiplyNumbers(20, 2));
-  }
-
-  showAge() {
-    return this.age + 10;
-  }
-
-  multiplyNumbers(num1: number, num2: number): number {
-    return num1 * num2;
-  }
-}
 
 
 
-*/
+
+// xport class UserComponent {
+//   // Properties
+//   personId;
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   address;
+//   foo: any;
+//   isGay: boolean;
+//   numArray: number[];
+//   strArray: string[];
+//   mixedArray: any[];
+//   myTuple: [string, number, boolean];
+//   unusable: void;
+//   u: undefined;
+//   n: null;
+
+//   // Methods
+//   constructor() {
+//     this.firstName = 'Keith';
+//     this.lastName = 'Fletcher';
+//     this.age = 46;
+
+//     this.address = {
+//       street: '23 Pudding Lane',
+//       city: 'Bedford',
+//       county: 'Bedfordshire'
+//     };
+
+//     this.foo = 'nice';
+//     this.numArray = [1, 2, 3];
+//     this.strArray = ['A', 'B', 'C'];
+//     this.mixedArray = [2, 'A', '67'];
+
+//     this.myTuple = ['hello', 7, false];
+//     this.unusable = undefined;
+//     this.u = undefined;
+//     this.n = null;
+
+//     console.log(this.multiplyNumbers(20, 2));
+//   }
+
+//   showAge() {
+//     return this.age + 10;
+//   }
+
+//   multiplyNumbers(num1: number, num2: number): number {
+//     return num1 * num2;
+//   }
+// }
